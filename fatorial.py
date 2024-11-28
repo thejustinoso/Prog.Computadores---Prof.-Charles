a7d12119ad780ca5f28beadb199892031e1c584e
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2024-11-27 21:52:29 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -1,9 +1,16 @@
+import sys
+
 numero = int(input('Informe um valor:'))
 
 contador = 1
 fatorial = 1
 
-while contador <= numero:
-    fatorial = fatorial * contador
-    contador = contador + 1 
-print (f'O resultado é: {fatorial}')
+if numero < 0:
+    SystemExit
+    print('Informe um número positivo')
+
+else:
+    while contador <= numero:
+        fatorial *= contador
+        contador += 1 
+    print (f'{numero}! = {fatorial}')
